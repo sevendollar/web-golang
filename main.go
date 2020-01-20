e32468d00f203a51379d3b74df24c6c1319f97a4
sevendollar <sevendollar@gmail.com>
2020-01-20 15:15:43 +0800

    add gap between time and contents

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	hostname, _ := os.Hostname()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Println(time.Now().Format("2006-01-02 15:04:05 Monday") + hostname)
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05 Monday") + " " + hostname)
 		fmt.Fprintln(w, hostname)
 	})
 	log.Fatal(http.ListenAndServe(":80", nil))
