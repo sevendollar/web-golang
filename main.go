2391e5ed219918b528cff953b2d5fadfae3aa2e4
sevendollar <sevendollar@gmail.com>
2020-01-20 15:06:56 +0800

    time format fixed

--- main.go
+++ main.go
@@ -10,11 +10,10 @@ import (
 
 func main() {
 	hostname, _ := os.Hostname()
-	currentTime := time.Now()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprintln(os.Stdout, currentTime.Format("2006-01-02 15:04:05 Monday")+hostname)
-		fmt.Fprintln(w, currentTime.Format("2006-01-02 15:04:05 Monday")+hostname)
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05 Monday") + hostname)
+		fmt.Fprintln(w, hostname)
 	})
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
