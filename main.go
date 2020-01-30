92fe84a70f2e68e954bab3199ed989e5b5e8dd99
sevendollar <sevendollar@gmail.com>
2020-01-30 16:34:37 +0800

    test k8s livenessProbe

--- main.go
+++ main.go
@@ -11,9 +11,19 @@ import (
 func main() {
 	hostname, _ := os.Hostname()
 
+	count := 0
+
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		if count >= 5 {
+			w.WriteHeader(404)
+			fmt.Println("users visit count hits the limit.")
+			return
+		}
 		fmt.Println(time.Now().Format("2006-01-02 15:04:05 Monday") + " " + hostname)
 		fmt.Fprintln(w, hostname)
+
+		count++
 	})
+	fmt.Println("servcie starting...")
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
