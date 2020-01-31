7da9b7167111956fd8af8b06bc7293e41ca02c3e
sevendollar <sevendollar@gmail.com>
2020-01-31 10:36:17 +0800

    comments  user visit count

--- main.go
+++ main.go
@@ -11,18 +11,18 @@ import (
 func main() {
 	hostname, _ := os.Hostname()
 
-	count := 0
+	// count := 0
 
-	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		if count >= 5 {
-			w.WriteHeader(404)
-			fmt.Println("users visit count hits the limit.")
-			return
-		}
-		fmt.Println(time.Now().Format("2006-01-02 15:04:05 Monday") + " " + hostname)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// if count >= 5 {
+		// 	w.WriteHeader(500)
+		// 	fmt.Println("users visit count hits the limit.")
+		// 	return
+		// }
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05 Monday") + " " + hostname + " " + r.UserAgent())
 		fmt.Fprintln(w, hostname)
 
-		count++
+		// count++
 	})
 	fmt.Println("servcie starting...")
 	log.Fatal(http.ListenAndServe(":80", nil))
